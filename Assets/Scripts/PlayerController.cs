91e2ec336083a208a90a70459774ee1ed1d54aac
EthanT <ethanmtolentino@gmail.com>
2021-07-21 03:55:49 +1000

    Polish Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,14 +12,17 @@ public class PlayerController : MonoBehaviour
     [HideInInspector]
     public bool isDead;
 
+    private Vector3 screenBounds;
 
     void Start()
     {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         lives = 5;
     }
 
     void Update()
     {
+
         //WIll stop the player and move to the restart scene if lives reaches 0
         if (lives != 0)
         {
@@ -44,6 +47,23 @@ public class PlayerController : MonoBehaviour
         {
             Destroy(gameObject);
             SceneManager.LoadScene(3);
+        }
+        if(transform.position.x >= 2.2f)
+        {
+            transform.position = new Vector3(2.2f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x <= -9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z >= 5.1f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 5.1f);
+        }
+        else if (transform.position.z <= -5.1f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -5.1f);
         }
     }
 }
